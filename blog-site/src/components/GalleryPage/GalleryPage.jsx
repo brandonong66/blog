4b5f5f1b384303fb967dd153672cd36775c90646
Brandon Ong <brandonong66@gmail.com>
2022-07-07 15:09:40 -0700

    fixed import error

--- blog-site/src/components/GalleryPage/GalleryPage.jsx
+++ blog-site/src/components/GalleryPage/GalleryPage.jsx
@@ -1,7 +1,6 @@
 import { Container } from "@mui/material"
 import React from "react"
 import ImageGrid from "./ImageGrid/"
-import ImageGrid2 from "./ImageGrid/ImageGrid2"
 function GalleryPage() {
   return (
     <Container>
