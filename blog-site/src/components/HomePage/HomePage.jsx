ed06ffc5ba30d0da89a70bb336ba925dbe4faa27
Brandon Ong <brandonong66@gmail.com>
2022-10-04 19:01:04 -0700

    coming soon for home page

--- blog-site/src/components/HomePage/HomePage.jsx
+++ blog-site/src/components/HomePage/HomePage.jsx
@@ -4,45 +4,46 @@ import {
   Card,
   CardContent,
   Container,
+  Link,
   Typography,
 } from "@mui/material"
 import React from "react"
 import PostCard from "./PostCard/PostCard"
-import { styled, ThemeProvider, createTheme } from "@mui/material/styles"
+import ConstructionIcon from "@mui/icons-material/Construction"
 
 function HomePage() {
   return (
-    <Container sx={{ pt: "1rem" }}>
-      <Grid container rowSpacing={1} maxHeight={{ xs: "25%" }}>
-        <Grid item xs={12}>
-          <PostCard />
-        </Grid>
-        <Grid item xs={12}>
-          <PostCard />
-        </Grid>
-        <Grid item xs={12}>
-          <PostCard />
-        </Grid>
-        <Grid item xs={12}>
-          <PostCard />
-        </Grid>
-        <Grid item xs={12}>
-          <PostCard />
-        </Grid>
-        <Grid item xs={12}>
-          <PostCard />
-        </Grid>
-        <Grid item xs={12}>
-          <PostCard />
-        </Grid>
-        <Grid item xs={12}>
-          <PostCard />
-        </Grid>
-        <Grid item xs={12}>
-          <PostCard />
-        </Grid>
-      </Grid>
-    </Container>
+    <div>
+      <Container
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+        }}
+      >
+        <ConstructionIcon
+          sx={{
+            width: "30%",
+            height: "30%",
+          }}
+        />
+      </Container>
+      <Typography
+        variant="h3"
+        sx={{
+          textAlign: "center",
+        }}
+      >
+        Coming Soon
+      </Typography>
+      <Typography
+        variant="body1"
+        sx={{
+          textAlign: "center",
+        }}
+      >
+        Checkout the 24v swap guide or gallery instead
+      </Typography>
+    </div>
   )
 }
 
